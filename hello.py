87843f99db2f120d5e229956e278a0db88fa9306
Moira Leon <moira.leon2@gmail.com>
2021-09-05 12:31:52 -0600

    Strings, Input and Output

--- hello.py
+++ hello.py
@@ -3,3 +3,12 @@
 # 10+20+30
 # quit()
 
+print ("Sarah")
+hello = "Hello"
+name = "Sarah"
+greeting = hello +" "+ name
+print(greeting)
+
+my_name =input("What's your name?") #or ("What's your name?\n") \n means new line so cursor will take input on new line
+
+print("Hello" +" "+ my_name)
